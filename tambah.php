2190be316ad91ae87798a8a0a526e2616a9f2dcd
PuddingJay <muhammadwindu27@gmail.com>
2022-01-09 20:19:01 +0700

    Ada yang salah

--- tambah.php
+++ tambah.php
@@ -27,12 +27,12 @@ if (isset($_POST["submit"])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <title>Tambah Data Mahasiswa</title>
+    <title>Tambah Data Karyawan</title>
 </head>
 
 <body>
 
-    <h1>Tambah Mahasiswa</h1>
+    <h1>Tambah Data</h1>
     <div class="containerFormTambah">
         <div class="formTambah">
             <form action="" method="post">
